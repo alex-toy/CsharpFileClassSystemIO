0cdc708439b31d1f488f9164dbd31320ee22136c
alex-toy <alexei.80@hotmail.fr>
2023-01-23 21:05:12 +0100

    init

---
+++ CsharpBasics/CsharpBasics/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string path = @"C:\source\CSsharpPackages\CsharpFileClassSystemIO\Folders\test.txt";
+File.Create(path);
