b84ab67f357ddc1a93cf0436e5e75e76744e7ee2
alex-toy <alexei.80@hotmail.fr>
2023-01-28 09:20:42 +0100

    methods

---
+++ CsharpBasics/CsharpBasics/FileHelper.cs
@@ -0,0 +1,20 @@
+﻿namespace CsharpBasics
+{
+    public static class FileHelper
+    {
+        public static void Append(string path, string content)
+        {
+            File.AppendAllText(path, content);
+        }
+
+        public static void AppendAllLines(string path, string[] lines)
+        {
+            File.AppendAllLines(path, lines);
+        }
+
+        public static async Task AppendAllTextAsync(string path, string content)
+        {
+            await File.AppendAllTextAsync(path, content);
+        }
+    }
+}
--- CsharpBasics/CsharpBasics/Program.cs
+++ CsharpBasics/CsharpBasics/Program.cs
@@ -1,5 +1,46 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using CsharpBasics;
+using System.Text;
 
 string path = @"C:\source\CSsharpPackages\CsharpFileClassSystemIO\Folders\test.txt";
-File.Create(path);
+
+//File.Create(path, 100, FileOptions.Asynchronous);
+
+
+//string message = "Alex le BG.";
+//await File.WriteAllBytesAsync(path, Encoding.ASCII.GetBytes(message));
+
+
+//IEnumerable<string> messages = new[]
+//{
+//    "Follow me on my adventures",
+//    "I love to code"
+//};
+//await File.WriteAllLinesAsync(path, messages);
+
+
+//var content = await File.ReadAllTextAsync(path);
+//Console.WriteLine(content);
+
+//string[] lines = await File.ReadAllLinesAsync(path);
+//foreach (var line in lines)
+//{
+//    Console.WriteLine(line);
+//}
+
+//File.Delete(path);
+
+//FileHelper.Append(path, "\n Welcome");
+
+//IEnumerable<string> messages = new[]
+//{
+//    "Follow me on my adventures",
+//    "I love to code"
+//};
+//FileHelper.AppendAllLines(path, messages.ToArray());
+
+//await FileHelper.AppendAllTextAsync(path, "alexei and his team");
+
+string pathDest = @"C:\source\CSsharpPackages\CsharpFileClassSystemIO\Folders\testCopy.txt";
+File.Copy(path, pathDest);
+
